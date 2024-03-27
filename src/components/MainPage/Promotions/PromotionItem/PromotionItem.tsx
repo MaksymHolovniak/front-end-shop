b193984f9b20397ca3c0f310c824b90b6d4da175
MaksymHolovniak <maksum.golovnyak@gmail.com>
2024-03-27 13:59:55 +0200

    Modified product card. Delete not used component

--- src/components/MainPage/Promotions/PromotionItem/PromotionItem.tsx
+++ src/components/MainPage/Promotions/PromotionItem/PromotionItem.tsx
@@ -1,6 +1,7 @@
 import s from './PromotionItem.module.css'
 import PromotionProductImg1 from './../../../../assets/PromotionProductImg1.png'
-
+import {Rate} from "antd";
+import starRatting from './../../../../assets/icons/starRatting.png'
 const PromotionItem = () => {
     return (
         <div className={s.content}>
@@ -9,11 +10,16 @@ const PromotionItem = () => {
             </div>
             <div className={s.description}>
                 <div className={s.price}>
-                    <div className={s.priceNumber}><p>44,50 ₽</p><p>50,50 ₽ </p></div>
+                    <div className={s.priceNumber}><p className={s.priceNumberCard}>44,50 ₽</p><p className={s.priceNumberStandart}>50,50 ₽ </p></div>
                     <div className={s.priceText}><p>С картой</p><p>Обычная</p></div>
                 </div>
+                <div className={s.descriptionText}>
+                    <p>Г/Ц Блинчики с мясом вес, Россия</p>
+                    <Rate disabled defaultValue={2} character={<img src={starRatting} alt={'starRatting'} />} className={s.ratting} />
+                </div>
+                <button className={s.basketButton}>В корзину</button>
+                </div>
             </div>
-        </div>
     )
 }
 
